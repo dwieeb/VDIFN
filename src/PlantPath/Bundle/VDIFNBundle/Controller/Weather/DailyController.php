374f65975bc1d6028248389a6ae8bbc441d3d35d
Daniel Imhoff <dwieeb@gmail.com>
2014-02-16 18:37:09 -0600

    Adding controller for getting daily weather data (disease severity values) at a given time and lat/long bounding box.

---
+++ src/PlantPath/Bundle/VDIFNBundle/Controller/Weather/DailyController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PlantPath\Bundle\VDIFNBundle\Controller\Weather;
+
+use PlantPath\Bundle\VDIFNBundle\Geo\Point;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use PlantPath\Bundle\VDIFNBundle\Entity\Weather\Daily;
+
+/**
+ * Weather\Daily controller.
+ *
+ * @Route("/weather/daily")
+ */
+class DailyController extends Controller
+{
+    /**
+     * Finds and displays a Weather\Daily entity.
+     *
+     * @Route("/{day}/{nwLat}/{nwLong}/{seLat}/{seLong}", name="weather_daily_bounding_box")
+     * @Method("GET")
+     */
+    public function boundingBoxAction(\DateTime $day, $nwLat, $nwLong, $seLat, $seLong)
+    {
+        $entity = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('PlantPathVDIFNBundle:Weather\Daily')
+            ->getWithinBoundingBox($day, new Point($nwLat, $nwLong), new Point($seLat, $seLong));
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find.');
+        }
+
+        return JsonResponse::create($entity);
+    }
+}
--- src/PlantPath/Bundle/VDIFNBundle/Repository/Weather/DailyRepository.php
+++ src/PlantPath/Bundle/VDIFNBundle/Repository/Weather/DailyRepository.php
@@ -23,4 +23,31 @@ class DailyRepository extends EntityRepository
             'longitude' => $point->getLongitude(),
         ]);
     }
+
+    /**
+     * Find daily weather data given a datetime and bounding box.
+     *
+     * @param  DateTime $dt
+     * @param  Point    $nw
+     * @param  Point    $se
+     * @param  array    $projection
+     *
+     * @return PlantPath\Bundle\VDIFNBundle\Entity\Weather\Daily
+     */
+    public function getWithinBoundingBox(\DateTime $dt, Point $nw, Point $se, array $projection = ['d.latitude', 'd.longitude', 'd.dsv'])
+    {
+        return $this
+            ->createQueryBuilder('d')
+            ->select($projection)
+            ->where('d.latitude BETWEEN :s AND :n')
+            ->andWhere('d.longitude BETWEEN :w AND :e')
+            ->andWhere('d.time = :dt')
+            ->setParameter('n', $nw->getLatitude())
+            ->setParameter('e', $se->getLongitude())
+            ->setParameter('s', $se->getLatitude())
+            ->setParameter('w', $nw->getLongitude())
+            ->setParameter('dt', $dt)
+            ->getQuery()
+            ->getResult();
+    }
 }
