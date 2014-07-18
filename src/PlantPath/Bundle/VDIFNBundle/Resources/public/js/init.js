27bf1fcaf9d9b616be6ed0ea534cd0bd9e3ac35a
Daniel Imhoff <dwieeb@gmail.com>
2014-07-18 18:14:12 -0500

    adding tooltips for each level of likelihood of diseases

--- src/PlantPath/Bundle/VDIFNBundle/Resources/public/js/init.js
+++ src/PlantPath/Bundle/VDIFNBundle/Resources/public/js/init.js
@@ -67,6 +67,56 @@ var Interface = new vdifn.Interface(
         Interface.closeTooltip();
     });
 
+    google.maps.event.addDomListener(document.getElementById('dsv-very-high'), 'mouseover', function(event) {
+        var content = document.createElement('div');
+        content.innerHTML = 'Very high likelihood of disease<br />(accumulated DSVs &ge; 20)';
+        Interface.openTooltip(document.getElementById('dsv-very-high'), content);
+    });
+
+    google.maps.event.addDomListener(document.getElementById('dsv-very-high'), 'mouseout', function(event) {
+        Interface.closeTooltip();
+    });
+
+    google.maps.event.addDomListener(document.getElementById('dsv-high'), 'mouseover', function(event) {
+        var content = document.createElement('div');
+        content.innerHTML = 'High likelihood of disease<br />(15 &le; accumulated DSVs &lt; 20)';
+        Interface.openTooltip(document.getElementById('dsv-high'), content);
+    });
+
+    google.maps.event.addDomListener(document.getElementById('dsv-high'), 'mouseout', function(event) {
+        Interface.closeTooltip();
+    });
+
+    google.maps.event.addDomListener(document.getElementById('dsv-medium'), 'mouseover', function(event) {
+        var content = document.createElement('div');
+        content.innerHTML = 'Medium likelihood of disease<br />(10 &le; accumulated DSVs &lt; 15)';
+        Interface.openTooltip(document.getElementById('dsv-medium'), content);
+    });
+
+    google.maps.event.addDomListener(document.getElementById('dsv-medium'), 'mouseout', function(event) {
+        Interface.closeTooltip();
+    });
+
+    google.maps.event.addDomListener(document.getElementById('dsv-low'), 'mouseover', function(event) {
+        var content = document.createElement('div');
+        content.innerHTML = 'Low likelihood of disease<br />(5 &le; accumulated DSVs &lt; 10)';
+        Interface.openTooltip(document.getElementById('dsv-low'), content);
+    });
+
+    google.maps.event.addDomListener(document.getElementById('dsv-low'), 'mouseout', function(event) {
+        Interface.closeTooltip();
+    });
+
+    google.maps.event.addDomListener(document.getElementById('dsv-very-low'), 'mouseover', function(event) {
+        var content = document.createElement('div');
+        content.innerHTML = 'Very low likelihood of disease<br />(0 &le; accumulated DSVs &lt; 5)';
+        Interface.openTooltip(document.getElementById('dsv-very-low'), content);
+    });
+
+    google.maps.event.addDomListener(document.getElementById('dsv-very-low'), 'mouseout', function(event) {
+        Interface.closeTooltip();
+    });
+
     google.maps.event.addDomListener(window, 'resize', Interface.resize.bind(Interface));
     google.maps.event.addListenerOnce(Interface.map, 'tilesloaded', Interface.tilesloaded.bind(Interface));
     google.maps.event.addDomListener(document.getElementById('error-button'), 'click', Interface.closeErrorOverlay.bind(Interface));
