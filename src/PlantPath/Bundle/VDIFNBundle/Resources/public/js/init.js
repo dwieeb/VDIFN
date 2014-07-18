0557e8b570e3b275dd75a89cd9e87bfb5b75b78a
Daniel Imhoff <dwieeb@gmail.com>
2014-07-18 18:02:43 -0500

    moving code around

--- src/PlantPath/Bundle/VDIFNBundle/Resources/public/js/init.js
+++ src/PlantPath/Bundle/VDIFNBundle/Resources/public/js/init.js
@@ -30,18 +30,6 @@ var Interface = new vdifn.Interface(
         field: document.getElementById('datepicker-end')
     });
 
-    var onDateRangeSelect = function(event) {
-        Interface.drawDateRange(Interface.startPicker.getDate(), Interface.endPicker.getDate(), onDateRangeDataLoad);
-    };
-
-    var onDateRangeDataLoad = function(success) {
-        Interface.closeLoadingOverlay();
-
-        if (!success) {
-            Interface.openErrorOverlay("Could not load weather data for the date range specified.");
-        }
-    };
-
     // Events
     if (vdifn.parameters.debug) {
         google.maps.event.addListener(Interface.map, 'click', function(event) {
@@ -49,20 +37,32 @@ var Interface = new vdifn.Interface(
         });
     }
 
-    google.maps.event.addDomListener(document.getElementById('datepicker-select'), 'click', onDateRangeSelect);
+    google.maps.event.addDomListener(document.getElementById('datepicker-select'), 'click', function(event) {
+        Interface.drawDateRange(Interface.startPicker.getDate(), Interface.endPicker.getDate(), function(success) {
+            Interface.closeLoadingOverlay();
+
+            if (!success) {
+                Interface.openErrorOverlay("Could not load weather data for the date range specified.");
+            }
+        });
+    });
+
     google.maps.event.addDomListener(document.getElementById('datepicker-start-information'), 'mouseover', function(event) {
         var content = document.createElement('div');
         content.innerHTML = 'Date of Emergence/Last Fungicide Application';
         Interface.openTooltip(document.getElementById('datepicker-start-information'), content);
     });
+
     google.maps.event.addDomListener(document.getElementById('datepicker-start-information'), 'mouseout', function(event) {
         Interface.closeTooltip();
     });
+
     google.maps.event.addDomListener(document.getElementById('datepicker-end-information'), 'mouseover', function(event) {
         var content = document.createElement('div');
         content.innerHTML = 'Date through which disease severity values are accumulated';
         Interface.openTooltip(document.getElementById('datepicker-end-information'), content);
     });
+
     google.maps.event.addDomListener(document.getElementById('datepicker-end-information'), 'mouseout', function(event) {
         Interface.closeTooltip();
     });
@@ -70,9 +70,9 @@ var Interface = new vdifn.Interface(
     google.maps.event.addDomListener(window, 'resize', Interface.resize.bind(Interface));
     google.maps.event.addListenerOnce(Interface.map, 'tilesloaded', Interface.tilesloaded.bind(Interface));
     google.maps.event.addDomListener(document.getElementById('error-button'), 'click', Interface.closeErrorOverlay.bind(Interface));
-    google.maps.event.trigger(window, 'resize');
 
     // Initialization
-    onDateRangeSelect();
+    google.maps.event.trigger(window, 'resize');
+    google.maps.event.trigger(document.getElementById('datepicker-select'), 'click');
     Interface.drawStations();
 })();
