5cec411a416f7a50b99518e0106c76c3c061fec2
Daniel Imhoff <dwieeb@gmail.com>
2015-02-12 20:31:57 -0600

    adding tests for DegreeDay

---
+++ src/PlantPath/Bundle/VDIFNBundle/Tests/Geo/DegreeDayTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace PlantPath\Bundle\VDIFNBundle\Tests\Geo;
+
+use PlantPath\Bundle\VDIFNBundle\Geo\DegreeDay;
+
+class DegreeDayTests extends \PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+        DegreeDay::create(10.0, 80);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testCreateInvalidBaseTemperature()
+    {
+        DegreeDay::create("a", 80);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testCreateInvalidAverageDailyTemplerature()
+    {
+        DegreeDay::create(10.0, "a");
+    }
+
+    public function testCalculate()
+    {
+        $dd = DegreeDay::create(10.0, 80);
+        $this->assertEquals($dd->calculate(), 70);
+    }
+
+    public function testCalculateZero()
+    {
+        $dd = DegreeDay::create(10.0, 10);
+        $this->assertEquals($dd->calculate(), 0);
+    }
+
+    public function testCalculateAverageLessThan()
+    {
+        $dd = DegreeDay::create(10.0, 0);
+        $this->assertEquals($dd->calculate(), 0);
+    }
+}
