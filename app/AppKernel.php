4be870c459db001a3fed403cff012facec7666f1
Daniel Imhoff <dwieeb@gmail.com>
2015-02-20 02:03:08 -0600

    integrating FOS User Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new PlantPath\Bundle\VDIFNBundle\PlantPathVDIFNBundle(),
         );
 
---
+++ src/PlantPath/Bundle/VDIFNBundle/Entity/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace PlantPath\Bundle\VDIFNBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer", nullable=false)
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+}
